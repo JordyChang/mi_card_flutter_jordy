4770380367028206df32bbc0256e8deec02b33ac
Jordy <btbict.jordy@gmail.com>
2019-07-16 05:13:29 +0800

    Business_card

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,99 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/jordychang.jpg'),
+              ),
+              Text(
+                'Jordy Chang',
+                style: TextStyle(
+                  fontFamily: 'Acme',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'PROJECT MANAGER',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 12.0,
+                  color: Colors.yellow,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        color: Colors.black,
+                        size: 30.0,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        '+8615501163807',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 15.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.email,
+                        color: Colors.black,
+                        size: 30.0,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        'Btbict.jordy@gmail.com',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 15.0,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
